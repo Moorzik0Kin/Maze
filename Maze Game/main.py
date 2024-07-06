16f73dfd91ab0407b553d4ac633fd014d169f6f3
Moorzik0Kin <161145033+Moorzik0Kin@users.noreply.github.com>
2024-07-06 17:30:11 +0300

    main.py

--- Maze Game/main.py
+++ Maze Game/main.py
@@ -1,5 +1,7 @@
 import pygame
+pygame.init()
 
+from time import sleep
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
@@ -17,17 +19,24 @@ window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Maze")
 background = pygame.transform.scale(pygame.image.load("background.jpg"), (width, height))
 
+final = GameSprite('treasure.png', 600, 400, 0)
+monster = GameSprite('cyborg.png', 470, 430, 2)
 player = GameSprite('hero.png', 5, 420, 4)
-monster = GameSprite('cyborg.png', 620, 280, 2)
-final = GameSprite('treasure.png', 580, 420, 0)
 
 
 wall1=pygame.Rect(250,150,25,400)
-wall2=pygame.Rect(250,150,25,400)
+wall2=pygame.Rect(0,300,130,25)
+wall3=pygame.Rect(125,150,350,25)
+wall4=pygame.Rect(600,150,100,25)
+wall5=pygame.Rect(400,150,25,75)
+wall6=pygame.Rect(675,0,25,330)
+wall7=pygame.Rect(400,320,300,25)
+wall8=pygame.Rect(250,0,450,25)
+wall9=pygame.Rect(250,475,100,25)
+wall10=pygame.Rect(300,125,25,25)
+wall11=pygame.Rect(450,25,25,25)
 
-
-
-
+walls = [wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9, wall10, wall11]
 
 
 game = True
@@ -36,13 +45,19 @@ clock = pygame.time.Clock()
 pygame.mixer.init()
 pygame.mixer.music.load('jungles.ogg')
 # pygame.mixer.music.play()
+kick = pygame.mixer.Sound("kick.ogg")
+money = pygame.mixer.Sound("money.ogg")
 
 color_wall=(0,200,0)
 speed=3
 fi=0
 mo="l"
 
+font=pygame.font.Font(None, 20)
+hello = font.render("Prees F for speed 2x. And D for 1x", True, (0, 160, 0))
+text = pygame.Rect(430, 325, 100, 100)
 while game:
+    pygame.draw.rect(window, color_wall, text)
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             game = False
@@ -56,12 +71,16 @@ while game:
         player.rect.y+=speed
     if key[pygame.K_UP] and player.rect.y>0:
         player.rect.y-=speed
+    if key[pygame.K_f]:
+        speed=6
+    if key[pygame.K_d]:
+        speed=3
 
     if fi!=80 and mo=="l":
-        monster.rect.x-=1.8
+        monster.rect.y-=0.6
         fi+=1
     if fi!=-80 and mo=="r":
-        monster.rect.x+=1.8
+        monster.rect.y+=0.6
         fi-=1
     
     if fi==80:
@@ -76,7 +95,24 @@ while game:
     player.draw()
     monster.draw()
     final.draw()
-    pygame.draw.rect(window, color_wall,wall1)
+    # pygame.draw.rect(window, color_wall,wall1)
+    # pygame.draw.rect(window, color_wall,wall2)
+    for j in walls:
+        pygame.draw.rect(window, color_wall,j)
+        if player.rect.colliderect(j):
+            kick.play(3)
+            sleep(2)
+            game=False
+    
+    window.blit(hello, (430, 325))
+    if player.rect.colliderect(monster):
+        kick.play()
+        sleep(3.0)
+        game=False
+    if player.rect.colliderect(final):
+        money.play()
+        sleep(3.0)
+        game=False
 
     pygame.display.update()
     clock.tick(60)
